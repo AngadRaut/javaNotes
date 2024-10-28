ee416154b3a10b06988dc24f2f82f60d395cdaa3
Angad Raut <angadraut6493@gmail.com>
2024-10-28 20:15:32 +0530

    demo file added agin

--- gitPractice/src/Demo.java
+++ gitPractice/src/Demo.java
@@ -1,6 +1,10 @@
 public class Demo {
 
+    public void add(){
+        System.out.println("added this method after first commit");
+    }
     public static void main(String[] args) {
         System.out.println("main method");
+        System.out.println("i have make changes");
     }
 }
